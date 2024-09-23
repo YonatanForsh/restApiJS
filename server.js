88b332b0d901707034e9e925eeddb70f1c6803bf
Yonatan <yonatanforsh@gmail.com>
2024-09-23 11:01:55 +0300

    restart

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express"
+
+const app = express()
+const port = 7499
+
+
+app.get("/", (req, res) =>{
+    res.send("welcome to my server")
+})
+
+app.get("/amn", (req, res) =>{
+    res.send("welcome to my server")
+})
+
+app.get("/amn/:id", (req, res) =>{
+    res.send("welcome to my server")
+})
+
+app.get("/amn/summary", (req, res) =>{
+    res.send("welcome to my server")
+})
+
+app.post("/amn", (req, res) =>{
+    res.send("welcome to my server")
+})
+
+
+app.listen(port, () => {
+    console.log(`server started on port ${port} visit http://localhost:${port}`)
+    
+})
